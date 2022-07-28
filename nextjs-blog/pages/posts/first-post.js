9c0d36006a48649b289c4154ebff7fd8715e3e1d
oleh.voitynskyi <oleh.voitynskyi@devcom.com>
2022-07-28 18:46:18 +0300

    nextjs basic tutorial

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>Create Next super puper App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+      </Head>
+      <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={344} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+}
