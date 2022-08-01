8ddef0fc9d895a7993496c77d38c60db6e958aaf
oleh.voitynskyi <oleh.voitynskyi@devcom.com>
2022-08-01 09:48:54 +0300

    next-js-summary-big-project last changes

---
+++ nextjs-blog/pages/[meetupId]/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const MeetupDetails = () => {
+  return (
+    <Fragment>
+      <img src="" alt=""/>
+    </Fragment>
+  );
+};
+
+export default MeetupDetails;
---
+++ nextjs-blog/pages/new-meetup/index.js
@@ -0,0 +1,15 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetup() {
+  const addMeetupHandler = (data) => {
+    console.log(data);
+  }
+
+  return (
+    <>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  )
+};
+
+export default NewMeetup;
