1a95a62f2e35e258b3f117b97831487764bef8a3
Oleh <oleh.voitynskyi@devcom.com>
2022-09-24 11:19:57 +0300

    lerna-next init packages

---
+++ nextjs-blog/packages/frontend/next.config.js
@@ -0,0 +1,19 @@
+const withPlugins = require('next-compose-plugins')
+const withTM = require('next-transpile-modules')
+
+
+module.exports = async (phase) => {
+  const nextConfig = {
+    reactStrictMode: true,
+    productionBrowserSourceMaps: true
+  }
+  const defaultConfig = {}
+  return  withPlugins([withTM(['shared', "@marketplace/store"])], nextConfig)(phase, { defaultConfig });
+}
+
+// const nextConfig = {
+//   reactStrictMode: false,
+//   productionBrowserSourceMaps: true
+// }
+
+// module.exports = withPlugins([withTM(['shared', "@marketplace/store"])], nextConfig)
